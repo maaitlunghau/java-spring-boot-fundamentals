26fd71d31c8538f400a6add24adf75cf9cd48938
maaitlunghau <chunhau.py@gmail.com>
2026-07-28 16:57:18 +0700

    feat(blueprint): implement base entity

--- projects/10-spring-boot-blueprint/src/main/java/com/maaitlunghau/__spring_boot_blueprint/common/entity/BaseEntity.java
+++ projects/10-spring-boot-blueprint/src/main/java/com/maaitlunghau/__spring_boot_blueprint/common/entity/BaseEntity.java
@@ -1,5 +1,33 @@
 package com.maaitlunghau.__spring_boot_blueprint.common.entity;
 
-public class BaseEntity {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Version
+    private Long version;
 }
