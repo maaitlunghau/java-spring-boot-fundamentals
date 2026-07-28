539bd97ac076fe4ea3549767cada69a230ecb56d
maaitlunghau <chunhau.py@gmail.com>
2026-07-28 13:54:48 +0700

    chore: initialize new spring project - spring boot blueprint

---
+++ projects/10-spring-boot-blueprint/src/test/java/com/maaitlunghau/__spring_boot_blueprint/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaitlunghau.__spring_boot_blueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
