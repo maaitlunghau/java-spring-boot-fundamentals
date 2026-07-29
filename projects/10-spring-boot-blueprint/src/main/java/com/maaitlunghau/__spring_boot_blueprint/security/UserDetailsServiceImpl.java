5f9e76da94c2a7ef3af01e73cae006f602b81de7
maaitlunghau <chunhau.py@gmail.com>
2026-07-29 14:33:02 +0700

    feat(blueprint): add user details service for auth

---
+++ projects/10-spring-boot-blueprint/src/main/java/com/maaitlunghau/__spring_boot_blueprint/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.maaitlunghau.__spring_boot_blueprint.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.maaitlunghau.__spring_boot_blueprint.module.user.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+}
